16620f4a372fe2ecfe87c076a12039909de91578
RiteshYennuwar <yennuwarritesh@hotmail.com>
2024-04-02 18:37:37 +0530

    data validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.logging import logger
 
 STAGE_NAME = 'Data Ingestion stage'
@@ -7,7 +8,18 @@ try:
     logger.info(f'_-_-_->>> stage {STAGE_NAME} started <<<-_-_-_')
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(f'_-_-_->>> stage {STAGE_NAME} completed <<<-_-_-_')
+    logger.info(f'_-_-_-<<< stage {STAGE_NAME} completed >>>-_-_-_')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Data validation stage'
+
+try:
+    logger.info(f'_-_-_->>> stage {STAGE_NAME} started <<<-_-_-_')
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f'_-_-_-<<< stage {STAGE_NAME} completed >>>-_-_-_')
 except Exception as e:
     logger.exception(e)
     raise e
