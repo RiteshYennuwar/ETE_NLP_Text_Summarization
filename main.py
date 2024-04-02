4a03677d459e0ef124f68b119e29194e99f2df38
RiteshYennuwar <yennuwarritesh@hotmail.com>
2024-04-02 16:37:34 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.logging import logger
 
-logger.info('custom logging')
+STAGE_NAME = 'Data Ingestion stage'
+
+try:
+    logger.info(f'_-_-_->>> stage {STAGE_NAME} started <<<-_-_-_')
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f'_-_-_->>> stage {STAGE_NAME} completed <<<-_-_-_')
+except Exception as e:
+    logger.exception(e)
+    raise e
